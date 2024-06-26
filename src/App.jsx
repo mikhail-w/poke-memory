3e7428f6406747577d5b3376ef815f383403cb0d
mikhail <mikhail.waddell@gmail.com>
2024-06-26 05:22:09 -0400

    create card shuffle function

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+
+const cardImages = [
+  { src: '/public/img/helmet-1.png' },
+  { src: '/public/img/potion-1.png' },
+  { src: '/public/img/ring-1.png' },
+  { src: '/public/img/scroll-1.png' },
+  { src: '/public/img/shield-1.png' },
+  { src: '/public/img/sword-1.png' },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  // shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      // .map(card => ({ ...card, id: Math.floor(Math.random() * 13) }));
+      .map(card => ({ ...card }));
+
+    setCards(genRand(shuffledCards));
+    setTurns(0);
+  };
+
+  const genRand = cards => {
+    const arr = [];
+    const size = Object.keys(cards).length;
+    // console.log('Cards1: ', cards);
+
+    while (arr.length < size) {
+      let id = Math.floor(Math.random() * size) + 1;
+      if (arr.indexOf(id) === -1) arr.push(id);
+    }
+
+    // console.log('Array: ', arr);
+
+    let newCards = cards.reduce((arr, val, idx) => {
+      let newVal = { ...val, id: idx };
+      arr.push(newVal);
+      return arr;
+    }, []);
+
+    // console.log('New Cards: ', newCards);
+    return newCards;
+  };
+
+  console.log(cards, turns);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Poke-Memory</h1>
+      <button onClick={shuffleCards}>New Game</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
